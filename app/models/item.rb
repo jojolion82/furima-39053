7fd9b511c17ef5308415e2c1bcf62e382bd71af1
jojolion82 <kzo.1219.ymd@gmail.com>
2023-07-20 22:18:56 +0900

    修正コミット

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,7 +29,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  # ジャンルの選択が「--」の時は保存不可
+  # ジャンルの選択が「---」の時は保存不可
   with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :prefecture_id
